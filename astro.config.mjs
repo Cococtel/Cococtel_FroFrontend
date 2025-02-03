1c00aa6fd2ba98a7cd36a17266a8dc258c0a4528
Tomas Parra Monroy <57538837+Tomas1802@users.noreply.github.com>
2025-02-03 00:04:26 -0500

    removed vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: vercel()
 });
