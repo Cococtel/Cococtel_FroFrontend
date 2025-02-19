c20a77400241f1d74c394a2233f4beaec8953dec
Tomas Parra Monroy <tparra@unal.edu.co>
2025-02-19 17:54:31 -0500

    Auth Dtos and design fixes

---
+++ src/features/auth/dtos/registerDto.ts
@@ -0,0 +1,10 @@
+export interface UserRegistration {
+    name: string;
+    lastname: string;
+    phone: string;
+    email: string;
+    image: string;
+    username: string;
+    password: string;
+    type: string;
+}
